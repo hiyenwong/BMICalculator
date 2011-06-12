339039170324bfa4b5bce683a223d7af78726197
Hai Yan Wang <robinwong51@gmail.com>
2011-06-12 17:26:50 +0800

    Result Activity

---
+++ src/com/hyanwang/app/ResultActivity.java
@@ -0,0 +1,31 @@
+package com.hyanwang.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * PROJECT NAME: bmiCalculator
+ * @version Jun 12, 2011 5:12:44 PM
+ * 
+ *
+ * @author kut@an
+ *         gmail: robinwong51@gmail.com     
+ *         blog: http://kutanworld.tumblr.com/
+ * @date Jun 12, 2011
+ * 
+ * @comment:
+ *
+ */
+
+public class ResultActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.result);
+		
+		
+	}
+
+}

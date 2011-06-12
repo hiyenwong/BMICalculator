80c654b75a053a2c1366af689d1058aa3b4e9e69
Hai Yan Wang <robinwong51@gmail.com>
2011-06-12 16:31:14 +0800

    Interface Calculator

---
+++ src/com/hyanwang/app/Calculator_Interface.java
@@ -0,0 +1,19 @@
+package com.hyanwang.app;
+
+/**
+ * PROJECT NAME: bmiCalculator
+ * @version Jun 12, 2011 4:16:31 PM
+ * 
+ *
+ * @author kut@an
+ *         gmail: robinwong51@gmail.com     
+ *         blog: http://kutanworld.tumblr.com/
+ * @date Jun 12, 2011
+ * 
+ * @comment:计算方法接口
+ *
+ */
+
+public interface Calculator_Interface {
+	int bmiCal(float weighvalue,float heightvalue); //计算BMI值
+}
